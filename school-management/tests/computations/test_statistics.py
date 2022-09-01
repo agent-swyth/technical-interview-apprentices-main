6990cea3e95aa48f317c83d5310d4853159d989d
Swyth <professional.yt@protonmail.com>
2022-09-01 22:02:39 +0200

    add test_compute_student_topic_grades_mean beginning and skip for .vscode/

--- school-management/tests/computations/test_statistics.py
+++ school-management/tests/computations/test_statistics.py
@@ -1,10 +1,14 @@
 import pytest
 from school_management.computations.statistics import compute_group_grades
-from school_management.models.school import Group
-
+from school_management.models.school import Group, Grade
 
 @pytest.mark.skip("I'm too lazy to implement unit tests")
-def test_compute_student_topic_grades_mean():
+
+def test_compute_student_topic_grades_mean(
+    grades: list[Grade],
+    student_id: int,
+    topic_id: int
+):
     pass
 
 
