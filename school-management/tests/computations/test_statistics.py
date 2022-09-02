07251b527db5bc964d93184634958aa181521be2
Swyth <professional.yt@protonmail.com>
2022-09-02 11:55:54 +0200

    update test_compute_student_topic_grades_mean

--- school-management/tests/computations/test_statistics.py
+++ school-management/tests/computations/test_statistics.py
@@ -4,13 +4,13 @@ from src.school_management.models.school import Group, Grade
 from tests.conftest import group_all_passing
 
 # Use built-in decorator to enable parametrization of arguments for a test
-# @pytest.mark.parametrize("grades,topic_id,student_id,expected", test_list)
-def test_compute_student_topic_grades_mean(group_all_passing: Group):
-    ## Just use grades property in Group class
+@pytest.mark.parametrize("topic_id,student_id,expected", [(2, 1, 12.0), (1, 2, 16.0), pytest.param(1, 1, 10.4, marks=pytest.mark.xfail)])
+def test_compute_student_topic_grades_mean(group_all_passing: Group, topic_id, student_id, expected):
+    ## Just use grades property in Group class from imported conftest
     grades:Grade = group_all_passing.grades
     ## Focus on one topic and one student for testing
-    topic_id = 2
-    student_id = 1
+    # topic_id = 2
+    # student_id = 1
     # Initialize sum for average calcul
     sum_grades = 0
     number_grades = 0
@@ -19,9 +19,9 @@ def test_compute_student_topic_grades_mean(group_all_passing: Group):
         if grade.topic_id == topic_id and grade.student_id == student_id:
             sum_grades += grade.grade
             number_grades += 1
-    assert sum_grades / number_grades == 12
+    assert sum_grades / number_grades == expected
 
-# @pytest.mark.skip("I'm too lazy to implement unit tests")
+@pytest.mark.skip("I'm too lazy to implement unit tests")
 def test_compute_group_grades(group_all_passing: Group):
     result = compute_group_grades(group_all_passing)
 
